39070c98af8b1a481d71af73796428593bfb42e0
Aaron Maraya <aaronmaraya7@gmail.com>
2023-01-06 14:49:20 +0800

    Add cmath header and computation to solve for the mean
    -floor() the quotient of sum of data times frequency
    and frequency as mean value

--- measures_of_dispersion_solver.cpp
+++ measures_of_dispersion_solver.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(){
@@ -7,12 +8,20 @@ int main(){
     cin >> num_of_data;
     float data[num_of_data];
     float frequency[num_of_data];
+    float mean;
+    float data_times_frequency;
+    float sum_of_frequency;
     for(int i = 0; i < num_of_data; i++){
         cout << "\nData: ";
         cin >> data[i];
         cout << "frequency: ";
         cin >> frequency[i];
 
+        data_times_frequency += data[i] * frequency[i];
+        sum_of_frequency += frequency[i];
     }
+    mean = data_times_frequency / sum_of_frequency;
+    mean = floor(mean * 100 + 0.5) / 100;
+    cout << mean;
     return 0;
 }
