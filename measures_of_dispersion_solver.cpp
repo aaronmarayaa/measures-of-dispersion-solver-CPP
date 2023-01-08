1c7163194fe7db474d7703d064463a61534be2bf
Aaron Maraya <aaronmaraya7@gmail.com>
2023-01-08 09:21:26 +0800

    Add function to solve standard deviation and variance
    -standard_deviation_and_variance function solves for the
    standard deviation and variance
    
    -measure_of_dispersion_solver.cpp file

--- measures_of_dispersion_solver.cpp
+++ measures_of_dispersion_solver.cpp
@@ -2,6 +2,13 @@
 #include <cmath>
 using namespace std;
 
+void standard_deviation_and_variance(float sum_of_data_minus_mean_squared_times_frequency, float sum_of_frequency){
+    float standardDeviation = sqrt(sum_of_data_minus_mean_squared_times_frequency / (sum_of_frequency - 1));
+    cout << "\nstandard deivation = " <<  floor(standardDeviation * 100 + 0.5) / 100;
+    float variance = pow(standardDeviation, 2);
+    cout << "\nvaraiance = " << floor(variance * 100 + 0.5) / 100;
+}
+
 int main(){
     cout << "How many data? ";
     int num_of_data;
@@ -26,16 +33,22 @@ int main(){
     float data_minus_mean;
     float data_minus_mean_squared;
     float data_minus_mean_squared_times_frequency;
+    float sum_of_data_minus_mean_squared_times_frequency = 0;
     for(int k = 0; k < num_of_data; k++){
         data_minus_mean = data[k] - mean;
         data_minus_mean = floor(data_minus_mean * 100 + 0.5) / 100;
         data_minus_mean_squared = pow(data_minus_mean, 2);
         data_minus_mean_squared_times_frequency = data_minus_mean_squared * frequency[k];
         
-        cout << data[k] << "   -   " << frequency[k] 
-            << "   -   " << data_minus_mean << "   -   " 
-            << data_minus_mean_squared << "   -   " << data_minus_mean_squared_times_frequency << endl; 
+        cout << data[k] << "    -    " << frequency[k] 
+            << "    -    " << data_minus_mean << "    -    " 
+            << data_minus_mean_squared << "    -    " << data_minus_mean_squared_times_frequency << endl; 
 
+        sum_of_data_minus_mean_squared_times_frequency += data_minus_mean_squared_times_frequency;
     }
+    cout << "\nsum of (data - mean) * frequency = " << sum_of_data_minus_mean_squared_times_frequency << endl;
+    cout << "n = " << sum_of_frequency << endl;
+
+    standard_deviation_and_variance(sum_of_data_minus_mean_squared_times_frequency, sum_of_frequency);
     return 0;
 }
